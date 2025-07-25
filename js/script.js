1302e1a8685bd7517413f0170e84ce2192e4e198
daptra <dafarelauliyapk@gmail.com>
2025-07-25 10:45:21 +0700

    Add filtering functionality and task completion feature to Todo List

--- js/script.js
+++ js/script.js
@@ -14,7 +14,8 @@ function addTask() {
 
     tasks.push({
         title: taskInput.value,
-        date: dateInput.value
+        date: dateInput.value,
+        completed: false,
     });
 
     console.log("Task added:", taskInput.value, "on", dateInput.value);
@@ -31,23 +32,41 @@ function removeAllTask() {
 }
 
 function toggleFilter() {
-
+    const filter = document.getElementById("filter-select").value;
+    if (filter === "") return;
+    renderTasks(filter);
 }
 
-function renderTasks() {
-  // Function to render tasks on the page
-  const taskList = document.getElementById("todo-list");
-  taskList.innerHTML = ""; // Clear the current list
+function completedTask(index) {
+    // Function to toggle the completion status of a task
+    tasks[index].completed = true;
+    renderTasks();
+}
+function renderTasks(filter = "all") {
+    const taskList = document.getElementById("todo-list");
+    taskList.innerHTML = "";
 
-  tasks.forEach((task, index) => {
-    taskList.innerHTML += `
-      <li class="todo-item flex justify-between items-center bg-white p-4 mb-2">
-        <span>${task.title}</span>
-        <div>
-          <button class="px-[10px] py-[2px] bg-green-500 text-white rounded-md">Edit</button>
-          <button class="px-[10px] py-[2px] bg-red-500 text-white rounded-md" onclick="();">Delete</button>
-        </div>
-      </li>
-    `;
-  });
+    let filteredTasks = tasks;
+    if (filter === "completed") {
+        filteredTasks = tasks.filter(task => task.completed);
+    } else if (filter === "uncompleted") {
+        filteredTasks = tasks.filter(task => !task.completed);
+    }
+
+    if (filteredTasks.length === 0) {
+        taskList.innerHTML = "<p>No tasks available</p>";
+        return;
+    }
+
+    filteredTasks.forEach((task, index) => {
+        taskList.innerHTML += `
+            <li class="todo-item flex justify-between items-center bg-white p-4 mb-2">
+                <span>${task.title}</span>
+                <div>
+                    <button type="button" class="px-[10px] py-[2px] bg-green-500 text-white rounded-md" onclick="uncompletedTask(${index});">Uncompleted</button>
+                    <button class="px-[10px] py-[2px] bg-red-500 text-white rounded-md" onclick="removeAllTask(${index});">Delete</button>
+                </div>
+            </li>
+        `;
+    });
 }
